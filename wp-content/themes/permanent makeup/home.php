41c55b260b166f892aeada26504c24c34b36b313
halyna-kuzminchuk <halkuz01@easv365.dk>
2025-08-20 13:54:43 +0300

    home update

--- wp-content/themes/permanent makeup/home.php
+++ wp-content/themes/permanent makeup/home.php
@@ -1,2 +1,26 @@
 <?php get_header(); ?>
+<div class="container">
+    <div class="content">
+        <?php if(have_posts()): ?>
+            <?php while(have_posts()): the_post() ?>
+                <?php
+                    $url = get_permalink();
+                    $title = get_the_title();
+                    $date = get_the_date();       // Get the date the post was written
+                    $author = get_the_author();   // Get the name of the user who wrote the post
+                    $excerpt = get_the_excerpt(); // Get the first 55 words of the blog entry
+                ?>
+                
+                <!-- HTML to print one blog entry preview -->
+                <div class="blog-preview">
+                    <h2><a href="<?php echo $url; ?>"><?php echo $title; ?></a></h2>
+                    <p><small>Written by <?php echo $author; ?> on <?php echo $date; ?></small></p>
+                    <p><?php echo $excerpt; ?></p>
+                </div>
+                
+            <?php endwhile; ?>
+        <?php endif; ?>
+    </div>
+</div>
+<?php get_footer(); ?>
 

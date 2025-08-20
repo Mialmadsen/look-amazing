e34668bfbeb3211410a7bca4f944348338ab9af9
halyna-kuzminchuk <halkuz01@easv365.dk>
2025-08-20 14:21:17 +0300

    categories

--- wp-content/themes/permanent makeup/single.php
+++ wp-content/themes/permanent makeup/single.php
@@ -6,6 +6,8 @@
             $date = get_the_date();       // Get the date the post was written
             $author = get_the_author();   // Get the name of the user who wrote the post
             $content = get_the_content(); // Get the full blog content
+            $categories = get_the_category();
+            $tags = get_the_tags();       // Get the tags associated with the post
         ?>
 
         <!-- HTML to print one blog entry preview -->
@@ -15,6 +17,20 @@
             <div class="post-content">
                 <?php echo $content; ?>
             </div>
+            <?php if($categories): ?>
+            <?php foreach($categories as $category): ?>
+                <a href="<?php echo get_category_link($category->term_id); ?>">
+                    <?php echo $category->name; ?>
+                </a>
+            <?php endforeach; ?>
+            <?php endif; ?>
+            <?php if($tags): ?>
+            <?php foreach($tags as $tag): ?>
+                <a href="<?php echo get_tag_link($tag->term_id); ?>">
+                    <?php echo $tag->name; ?>
+                </a>
+            <?php endforeach; ?>
+            <?php endif; ?>
         </div>
 
     <?php endwhile; ?>

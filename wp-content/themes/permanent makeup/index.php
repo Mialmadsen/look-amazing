5ee717f6d958113b0787eb3ec34cf8c557c86664
halyna-kuzminchuk <halkuz01@easv365.dk>
2025-08-27 13:46:00 +0300

    home link

--- wp-content/themes/permanent makeup/index.php
+++ wp-content/themes/permanent makeup/index.php
@@ -2,6 +2,10 @@
 
  <?php get_template_part('template-parts/index', 'testimonials'); ?>
  <p>Hello world</p>
+<a href="<?php echo get_permalink( get_option('page_for_posts') ); ?>" class="btn">
+    View All Posts
+</a>
+
 
 
 

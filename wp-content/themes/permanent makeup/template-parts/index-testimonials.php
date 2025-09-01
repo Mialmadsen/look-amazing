ac53b9700650e5af23570557f2b015d911f8f711
halyna-kuzminchuk <halkuz01@easv365.dk>
2025-09-01 11:36:39 +0300

    Age and date
    Comment section

--- wp-content/themes/permanent makeup/template-parts/index-testimonials.php
+++ wp-content/themes/permanent makeup/template-parts/index-testimonials.php
@@ -2,7 +2,7 @@
   <div class="testimonial-wrapper">
     <?php
     $args = array(
-      'post_type' => 'testimonial',
+      'post_type'      => 'testimonial',
       'posts_per_page' => 3
     );
     $query = new WP_Query($args);
@@ -11,23 +11,37 @@
       while ($query->have_posts()) : $query->the_post();
 
         $image = get_field('testimonial_image');
-        $name = get_field('testimonial_name');
-        $text = get_field('testimonial_body_text');
-    ?>
+        $name  = get_field('testimonial_name');
+        $text  = get_field('testimonial_body_text');
+        $age   = get_field('testimonial_age');
+        $date  = get_the_date();
+        ?>
+        
         <article class="testimonial-card">
           <div class="testimonial-header">
             <?php if (!empty($image)) : ?>
               <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($name); ?>" class="testimonial-image" />
             <?php endif; ?>
+
             <?php if (!empty($name)) : ?>
               <h3 class="testimonial-name"><?php echo esc_html($name); ?></h3>
             <?php endif; ?>
+
+            <?php if (!empty($age)) : ?>
+              <span class="testimonial-age">, <?php echo esc_html($age); ?></span>
+            <?php endif; ?>
           </div>
+
           <?php if (!empty($text)) : ?>
             <p class="testimonial-text"><?php echo wp_trim_words(wp_kses_post($text), 50, '...'); ?></p>
           <?php endif; ?>
+
+          <?php if (!empty($date)) : ?>
+            <p class="testimonial-date"><?php echo esc_html($date); ?></p>
+          <?php endif; ?>
         </article>
-    <?php
+
+        <?php
       endwhile;
       wp_reset_postdata();
     else :

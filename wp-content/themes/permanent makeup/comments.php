44ce0f22a560b6eb80fe0ac2e31ea9c858b1bdeb
halyna-kuzminchuk <halkuz01@easv365.dk>
2025-09-09 11:09:46 +0300

    comments

--- wp-content/themes/permanent makeup/comments.php
+++ wp-content/themes/permanent makeup/comments.php
@@ -1,19 +1,25 @@
-<div id="comments" classe="comments-area">
+<div id="comments" class="comments-area">
 
-<?php if (have_comments()) : ?>
-    <h2 class="comments-title">Comments</h2>
+  <?php if ( have_comments() ) : ?>
+    <h2 class="comments-title"><?php _e('Kommentarer', 'permanent-makeup'); ?></h2>
+
     <ul class="commentlist">
-        <?php wp_list_comments(); ?>
+      <?php wp_list_comments(); ?>
     </ul>
+
     <?php the_comments_navigation(); ?>
-<?php endif; ?>
+  <?php endif; ?>
 
-<?php if (comments_open()) : ?>
-    <?php comment_form(); ?>
-    <?php else : ?>
-        <p class="no-comments">Comments are closed.</p>
-<?php endif; ?>
-
-
-
+  <?php
+  if ( comments_open() ) {
+    comment_form([
+      'title_reply'       => __('Skriv en kommentar', 'permanent-makeup'),
+      'title_reply_to'    => __('Skriv et svar til %s', 'permanent-makeup'),
+      'label_submit'      => __('Send kommentar', 'permanent-makeup'),
+      'cancel_reply_link' => __('Annuller svar', 'permanent-makeup'),
+    ]);
+  } else {
+    echo '<p class="no-comments">'. __('Kommentarer er lukket.', 'permanent-makeup') .'</p>';
+  }
+  ?>
 </div>

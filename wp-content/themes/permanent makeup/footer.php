e098b069b64d5cfa9faf3d44996a6e1a34780058
Tove2812 <tovdeb01@easv365.dk>
2025-09-24 13:16:37 +0200

    Ved ikke helt

--- wp-content/themes/permanent makeup/footer.php
+++ wp-content/themes/permanent makeup/footer.php
@@ -49,11 +49,11 @@
 
     <!-- Sociale Medier Ikoner -->
   <div class="footer-socials">
-    <a href="<?php the_field('facebook_url', "option"); ?>" target="_blank" aria-label="Facebook">
+    <a href="<?php the_field('facebook_url', "option"); ?>" target="_blank" aria-label="Dette er et simpelt hvidt Facebook Icon, som linker til Facebook">
         <i class="fab fa-facebook-f"></i>
     </a>
 
-    <a href="<?php the_field('instagram_url', "option"); ?>" target="_blank" aria-label="Instagram">
+    <a href="<?php the_field('instagram_url', "option"); ?>" target="_blank" aria-label="Dette er et simpelt hvidt Instagram Icon, som linker til Instagram">
         <i class="fab fa-instagram"></i>
     </a>
 
